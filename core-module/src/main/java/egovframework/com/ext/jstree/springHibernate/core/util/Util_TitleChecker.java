440d107ca0dd2ddceb5b6e075a7227f0dbef95d2
LeeDongMin <admin@313.co.kr>
2022-10-15 12:43:29 +0900

    [SP-313] #comment 특수문자를 제거하는 로직을 유틸클래스로 제공하고 있었으나, 특정한 특수문자만을 거르기 위하여 우선 disable 하기로 한다. ( with. version up ) #close #time 1h +review SR @dongmin.lee
    [ Core-Library::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- core-module/src/main/java/egovframework/com/ext/jstree/springHibernate/core/util/Util_TitleChecker.java
+++ core-module/src/main/java/egovframework/com/ext/jstree/springHibernate/core/util/Util_TitleChecker.java
@@ -3,8 +3,8 @@ package egovframework.com.ext.jstree.springHibernate.core.util;
 public class Util_TitleChecker {
 
 	public static String StringReplace(String str) {
-		String match = "[^\uAC00-\uD7A3xfe0-9a-zA-Z\\s]";
-		str = str.replaceAll(match, " ");
+		//String match = "[^\uAC00-\uD7A3xfe0-9a-zA-Z\\s]";
+		//str = str.replaceAll(match, " ");
 		return str;
 	}
 }
