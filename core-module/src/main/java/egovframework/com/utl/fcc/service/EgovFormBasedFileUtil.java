b8c3fa309abedec7d8920b148367d2dfe290b60e
LeeDongMin <admin@313.co.kr>
2022-11-06 17:09:30 +0900

    [SP-313] #comment upload method extend #close #time 1h +review SR @dongmin.lee
    [ Core-Library::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- core-module/src/main/java/egovframework/com/utl/fcc/service/EgovFormBasedFileUtil.java
+++ core-module/src/main/java/egovframework/com/utl/fcc/service/EgovFormBasedFileUtil.java
@@ -179,6 +179,45 @@ public class EgovFormBasedFileUtil {
 		}
 	}
 
+	public static void downloadFile(HttpServletResponse response, String where, String serverSubPath, String physicalName, String contentType ,String original) throws Exception {
+		String downFileName = where + SEPERATOR + serverSubPath + SEPERATOR + physicalName;
+
+		File file = new File(EgovWebUtil.filePathBlackList(downFileName));
+
+		if (!file.exists()) {
+			throw new FileNotFoundException(downFileName);
+		}
+
+		if (!file.isFile()) {
+			throw new FileNotFoundException(downFileName);
+		}
+
+		byte[] b = new byte[BUFFER_SIZE];
+
+		original = original.replaceAll("\r", "").replaceAll("\n", "");
+		response.setContentType(contentType);
+		response.setHeader("Content-Disposition", "attachment; filename=\"" + convert(original) + "\";");
+		response.setHeader("Content-Transfer-Encoding", "binary");
+		response.setHeader("Pragma", "no-cache");
+		response.setHeader("Expires", "0");
+
+		BufferedInputStream fin = null;
+		BufferedOutputStream outs = null;
+
+		try {
+			fin = new BufferedInputStream(new FileInputStream(file));
+			outs = new BufferedOutputStream(response.getOutputStream());
+
+			int read = 0;
+
+			while ((read = fin.read(b)) != -1) {
+				outs.write(b, 0, read);
+			}
+		} finally {
+			EgovResourceCloseHelper.close(outs, fin);
+		}
+	}
+
 	public static void viewFile(HttpServletResponse response, String where, String serverSubPath, String physicalName, String mimeTypeParam) throws Exception {
 		String mimeType = mimeTypeParam;
 		String downFileName = where + SEPERATOR + serverSubPath + SEPERATOR + physicalName;
