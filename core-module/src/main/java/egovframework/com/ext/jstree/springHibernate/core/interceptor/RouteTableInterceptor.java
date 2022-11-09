b53210daaf7b2a6e192cff27026c9961937caea2
LeeDongMin <admin@313.co.kr>
2022-11-09 13:34:25 +0900

    [SP-313] #comment Hibernate Interceptor로 테이블 이름 변경 #close #time 1h +review SR @dongmin.lee
    [ Core-Library::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

---
+++ core-module/src/main/java/egovframework/com/ext/jstree/springHibernate/core/interceptor/RouteTableInterceptor.java
@@ -0,0 +1,36 @@
+package egovframework.com.ext.jstree.springHibernate.core.interceptor;
+
+import egovframework.com.ext.jstree.support.util.StringUtils;
+import org.hibernate.EmptyInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RouteTableInterceptor extends EmptyInterceptor {
+
+    private static final Logger logger = LoggerFactory.getLogger(RouteTableInterceptor.class);
+
+    @Override
+    public String onPrepareStatement(String sql) {
+
+        String prepedStatement = super.onPrepareStatement(sql);
+        try {
+            if (null == SessionUtil.getAttribute("replaceTableName")) {
+                logger.info("Normal JSTF Framework Excute");
+            } else {
+                String replaceTableName = (String) SessionUtil.getAttribute("replaceTableName");
+                logger.info("CustomInterceptor :: prepedStatement - before =>" + prepedStatement);
+                logger.info("CustomInterceptor :: replaceTableName =>" + replaceTableName);
+                if (StringUtils.isNotEmpty(replaceTableName)) {
+                    prepedStatement = prepedStatement.replaceAll("from T_ARMS_REQ", "from " + replaceTableName);
+                    logger.info("CustomInterceptor :: prepedStatement - after =>" + prepedStatement);
+                }
+                return prepedStatement;
+
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return prepedStatement;
+    }
+}
