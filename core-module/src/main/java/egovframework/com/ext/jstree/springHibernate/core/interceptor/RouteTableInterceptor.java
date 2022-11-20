9770cc9f85aab4e962101b7044a31e8a749780ab
LeeDongMin <admin@313.co.kr>
2022-11-20 13:28:03 +0900

    [SP-313] #comment 스위칭 디비 테이블 레프트 라이트 값이 이상하게 적용되는 점 때문에 세션 핸들링 로직을 변경하는 중. #close #time 1h +review SR @dongmin.lee
    [ Core-Library::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- core-module/src/main/java/egovframework/com/ext/jstree/springHibernate/core/interceptor/RouteTableInterceptor.java
+++ core-module/src/main/java/egovframework/com/ext/jstree/springHibernate/core/interceptor/RouteTableInterceptor.java
@@ -14,33 +14,60 @@ public class RouteTableInterceptor extends EmptyInterceptor {
     @Override
     public String onPrepareStatement(String sql) {
 
+        String prepedStatement = super.onPrepareStatement(sql);
+
         try {
             HttpServletRequest httpServletRequest = SessionUtil.getUrl();
             String servletPath = httpServletRequest.getServletPath();
+
+            ///auth-user/api/arms/reqAdd/T_ARMS_REQADD_145/getChildNode.do
+
+            if(StringUtils.contains(servletPath,"T_ARMS_REQADD_")){
+                if(StringUtils.contains(servletPath,"getMonitor.do")){
+                    String replaceTableName = (String) SessionUtil.getAttribute("getMonitor");
+                    return replaceStatement(prepedStatement, replaceTableName);
+                }
+                if(StringUtils.contains(servletPath,"getNode.do")){
+                    String replaceTableName = (String) SessionUtil.getAttribute("getNode");
+                    return replaceStatement(prepedStatement, replaceTableName);
+                }
+                if(StringUtils.contains(servletPath,"getChildNode.do")){
+                    String replaceTableName = (String) SessionUtil.getAttribute("getChildNode");
+                    return replaceStatement(prepedStatement, replaceTableName);
+                }
+                if(StringUtils.contains(servletPath,"getChildNodeWithParent.do")){
+                    String replaceTableName = (String) SessionUtil.getAttribute("getChildNodeWithParent");
+                    return replaceStatement(prepedStatement, replaceTableName);
+                }
+                if(StringUtils.contains(servletPath,"addNode.do")){
+                    String replaceTableName = (String) SessionUtil.getAttribute("addNode");
+                    return replaceStatement(prepedStatement, replaceTableName);
+                }
+                if(StringUtils.contains(servletPath,"updateNode.do")){
+                    String replaceTableName = (String) SessionUtil.getAttribute("updateNode");
+                    return replaceStatement(prepedStatement, replaceTableName);
+                }
+                if(StringUtils.contains(servletPath,"moveNode.do")){
+                    String replaceTableName = (String) SessionUtil.getAttribute("moveNode");
+                    return replaceStatement(prepedStatement, replaceTableName);
+                }
+            }
             logger.info("RouteTableInterceptor :: servletPath -> " + servletPath);
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-        String prepedStatement = super.onPrepareStatement(sql);
-        try {
-            if (null == SessionUtil.getAttribute("replaceTableName")) {
-                logger.info("Normal JSTF Framework Excute");
-            } else {
-                String replaceTableName = (String) SessionUtil.getAttribute("replaceTableName");
-                logger.info("CustomInterceptor :: prepedStatement - before =>" + prepedStatement);
-                logger.info("CustomInterceptor :: replaceTableName =>" + replaceTableName);
-                if (StringUtils.isNotEmpty(replaceTableName)) {
-                    prepedStatement = prepedStatement.replaceAll("T_ARMS_REQADD", replaceTableName);
-                    logger.info("CustomInterceptor :: prepedStatement - after =>" + prepedStatement);
-                }
-                return prepedStatement;
 
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+        return prepedStatement;
+    }
+
+    private String replaceStatement(String prepedStatement, String replaceTableName) {
+        logger.info("RouteTableInterceptor :: prepedStatement - before =>" + prepedStatement);
+        logger.info("RouteTableInterceptor :: replaceTableName =>" + replaceTableName);
+        if (StringUtils.isNotEmpty(replaceTableName)) {
+            prepedStatement = prepedStatement.replaceAll("T_ARMS_REQADD", replaceTableName);
+            logger.info("RouteTableInterceptor :: prepedStatement - after =>" + prepedStatement);
         }
-
         return prepedStatement;
     }
 }
