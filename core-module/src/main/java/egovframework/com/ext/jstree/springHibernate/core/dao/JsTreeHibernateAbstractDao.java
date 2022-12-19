71edb4e9ab1a1abf83e5929f1e6b60b4590fb8f2
LeeDongMin <admin@313.co.kr>
2022-12-19 21:06:21 +0900

    [SP-313] #comment ID Extract 로 인하여 동일 데이터를 중복처리하는 문제로 원복 처리 23.02.01 #close #time 1h +review SR @dongmin.lee
    [ Core-Library::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CI-Build] http://www.313.co.kr/bamboo
    [BuildManager] http://www.313.co.kr/spinnaker
    [CD-Deploy] http://www.313.co.kr/jenkins
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker] https://hub.docker.com/u/313devgrp

--- core-module/src/main/java/egovframework/com/ext/jstree/springHibernate/core/dao/JsTreeHibernateAbstractDao.java
+++ core-module/src/main/java/egovframework/com/ext/jstree/springHibernate/core/dao/JsTreeHibernateAbstractDao.java
@@ -226,7 +226,6 @@ public abstract class JsTreeHibernateAbstractDao<T extends JsTreeHibernateSearch
 
 	public List<T> getListWithoutPaging(T extractSearchDTO) {
 		DetachedCriteria detachedCriteria = createDetachedCriteria();
-		detachedCriteria.setResultTransformer(CriteriaSpecification.ROOT_ENTITY);
 		for (Order order : extractSearchDTO.getOrder()) {
 			detachedCriteria.addOrder(order);
 		}
